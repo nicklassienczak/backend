e21b472b0657ae438b65f155eb5eed378f9a3379
clbokea <clbo@kea.dk>
2023-09-26 14:52:23 +0200

    init

---
+++ src/test/java/ita3/car/CarsApplicationTests.java
@@ -0,0 +1,13 @@
+package ita3.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
